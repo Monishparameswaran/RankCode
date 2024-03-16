22150e67ee0b94f365f9cb0abc69cdcdb49d1e5c
Monishparameswaran <monishparameshwaran@gmail.com>
2024-03-16 10:58:00 +0530

    Modified to proper project  structure

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+  
+ return (
+    <div>
+      <h1>Welcome to the sona coding community! </h1>
+      <p>Let's build something great together.</p>
+      <div>
+         
+      </div>
+    </div>
+ )
+  
+}
+
+export default App
